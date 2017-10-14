c3c8fccca520761c3cd548bec85725b70de0b66b
Hong-She Liang <starofrainnight@gmail.com>
2017-10-14 19:56:08 +0800

    Bump version: 0.2.4 → 0.3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [
 
 setup(
     name='abhealer',
-    version='0.2.4',
+    version='0.3.0',
     description="abhealer",
     long_description=long_description,
     author="Hong-She Liang",
