d72a8264a96a00a1717c546f3d1d608649a3f744
Hong-She Liang <starofrainnight@gmail.com>
2017-11-03 15:15:25 +0800

    Added global logger for whole abhealer project

--- abhealer/abhealer.py
+++ abhealer/abhealer.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+
+import logging
+
+_logger = None
+
+
+def logger():
+    global _logger
+
+    if _logger:
+        return _logger
+
+    _logger = logging.getLogger(__name__)
+
+    return _logger
