6213c2d1a17541c2972ec945bc9e115fd968ee65
Hong-She Liang <starofrainnight@gmail.com>
2017-11-03 20:54:45 +0800

    Moved path related functions to pathutils module

--- abhealer/__main__.py
+++ abhealer/__main__.py
@@ -18,6 +18,9 @@ import stat
 import pwd
 import grp
 import copy
+from . import arecabackup
+from .pathutils import get_path_owner, get_path_group, chown, normal_path, \
+    compute_related_path
 from whichcraft import which
 
 
@@ -124,67 +127,6 @@ def get_trace_infos(proj_dir):
             trace_infos[infos[0]] = infos
 
     return trace_infos
-
-
-def normal_path(apath):
-    if sys.platform == 'win32':
-        return os.path.abspath(apath).replace("/", "\\")
-    else:
-        return os.path.abspath(apath).replace("\\", "/")
-
-
-def compute_related_path(link, target):
-    link = pathlib.Path(normal_path(str(link)))
-    target = pathlib.Path(normal_path(str(target)))
-
-    link_parents = [str(p) for p in reversed(link.parents)]
-    target_parents = [str(p) for p in reversed(target.parents)]
-
-    max_parents = min(len(link.parents), len(target.parents))
-    found_index = max_parents
-    for i in range(0, max_parents):
-        if link_parents[i] != target_parents[i]:
-            found_index = i
-            break
-
-    if len(link.parents) > len(target.parents):
-        return os.path.join(
-            "../" * (len(link.parents) - found_index), target.name)
-    elif found_index == max_parents:
-        return target.name
-    else:
-        # Don't join like this:
-        #
-        #    os.path.join(*target_parents[found_index:], target.name)
-        #
-        # This syntax will lead python below v3.4 report error : "SyntaxError:
-        # only named arguments may follow *expression"!
-        return os.path.join(
-            os.path.join(*target_parents[found_index:]), target.name)
-
-
-def get_path_owner(apath):
-    apath = str(apath)
-    if os.path.islink(apath):
-        return pwd.getpwuid(os.lstat(apath).st_uid).pw_name
-    else:
-        return pwd.getpwuid(os.stat(apath).st_uid).pw_name
-
-
-def get_path_group(apath):
-    apath = str(apath)
-    if os.path.islink(apath):
-        return grp.getgrgid(os.lstat(apath).st_gid).gr_name
-    else:
-        return grp.getgrgid(os.stat(apath).st_gid).gr_name
-
-
-def chown(apath, uid, gid):
-    apath = str(apath)
-    if os.path.islink(apath):
-        os.lchown(apath, uid, gid)
-    else:
-        os.chown(apath, uid, gid)
 
 
 def recover_dirs(is_dockerized, orig_dir, source_dir, dest_dir):
