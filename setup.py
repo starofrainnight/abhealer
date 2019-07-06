afa4319bdc0c7d1e4540119a621c475675fddf50
Hong-She Liang <starofrainnight@gmail.com>
2019-07-06 19:29:47 +0800

    Bump version: 0.5.1 → 0.5.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ tests_requires = [
 
 setup(
     name="abhealer",
-    version="0.5.1",
+    version="0.5.2",
     description="An Areca Backup helper program",
     long_description=long_description,
     author="Hong-She Liang",
